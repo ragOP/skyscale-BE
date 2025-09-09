e859eb862abfcf79d6b26bea83348c1a0a1f7269
mdehtasham11 <md.01219011922@ipu.ac.in>
2025-09-09 23:27:16 +0530

    feat: update order model

--- models/orderModel6.js
+++ models/orderModel6.js
@@ -1,22 +1,25 @@
 const { mongoose, Schema } = require("mongoose");
 
-const order6Schema = new Schema({
-  fullName: { type: String },
-  email: { type: String },
-  phone: { type: String },
-  address: { type: String },
-  dob: { type: Date },
-  items: [
-    {
-      title: { type: String },
-      price: { type: Number },
-    },
-  ],
-  orderId: { type: String },
-  status: { type: String },
-  razorPayOrderId: { type: String },
-  razorPayPaymentId: { type: String },
-  razorPaySignatureId: { type: String },
-});
+const order6Schema = new Schema(
+  {
+    fullName: { type: String },
+    email: { type: String },
+    phone: { type: String },
+    address: { type: String },
+    dob: { type: Date },
+    items: [
+      {
+        title: { type: String },
+        price: { type: Number },
+      },
+    ],
+    orderId: { type: String },
+    status: { type: String },
+    razorPayOrderId: { type: String },
+    razorPayPaymentId: { type: String },
+    razorPaySignatureId: { type: String },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Order6", order6Schema);
