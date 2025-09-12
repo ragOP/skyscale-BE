453b4fea408805bc77fdfa6a03e3ba8453302bf4
farish-jamal <farishjamal98@gmail.com>
2025-09-13 01:06:56 +0530

    feat: add success and details endpoints for order processing

--- routers/payU/index.js
+++ routers/payU/index.js
@@ -42,4 +42,43 @@ router.get("/pay", async (req, res) => {
   }
 });
 
+router.get("/pay-sister", async (req, res) => {
+  try {
+    const { 
+      amount, 
+      productinfo,
+      name,
+      email,
+      phone,
+      dateOfBirth,
+      gender,
+      placeOfBirth,
+      additionalProducts = [],
+    } = req.query;
+
+    const txnid = "TXN" + Date.now();
+
+    const paymentParams = {
+      txnid,
+      amount,
+      productinfo,
+      firstname: name,
+      phone: phone,
+      email: email,
+      surl: `http://localhost:9005/api/lander5/success?txnid=${txnid}&email=${email}&phone=${phone}`,
+      furl: `http://localhost:9005/api/lander5/success?txnid=${txnid}&email=${email}&phone=${phone}`,
+      udf1: dateOfBirth,
+      udf2: gender,
+      udf3: placeOfBirth,
+      udf4: additionalProducts,
+    };
+
+    const result = await payuClient.paymentInitiate(paymentParams);
+    return res.send(result);
+  } catch (err) {
+    console.error("PayU pay error:", err);
+    res.status(500).json({ success: false, message: "Payment init failed" });
+  }
+});
+
 module.exports = router;
